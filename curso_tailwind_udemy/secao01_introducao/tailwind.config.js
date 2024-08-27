d653494bae149555d11236806569d1659abc41bd
matcbv <matheuscbv23@gmail.com>
2024-08-27 01:40:56 -0300

    Adicao dos primeiros resumos sobre Tailwind

---
+++ curso_tailwind_udemy/secao01_introducao/tailwind.config.js
@@ -0,0 +1,9 @@
+// Com o tailwind.config.js, estaremos adicionando as configurações a serem utilizadas pelo tailwind.
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
