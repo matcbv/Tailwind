571e502033da3ed52a8c5f318ef2f6980ae034cd
matcbv <matheuscbv23@gmail.com>
2024-08-30 00:37:43 -0300

    Adicinado projeto clone do spotify

---
+++ curso_tailwind_udemy/secao07_clone_spotify/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundImage: theme => ({
+
+      })
+    },
+  },
+  plugins: [],
+}
+
